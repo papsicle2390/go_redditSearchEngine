d8c8ea1c586f8233830285e4913fb9d0421c5e4b
Jacob Kranz <jacob.kranz@gmail.com>
2014-04-17 15:06:42 -0700

    create todo at bottom of main.go

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-
-
 	"fmt"
-	
-
 )
 
 func main() {
-fmt.Println("hey")
-fmt.Println("fuck yea!")
+
 }
+
+/*
+
+STEPS
+	1) Scrape the site
+		a) Make a connection to the site
+		b) Get the info from connection (the text)
+		c) Transform the text into a struct
+	2) DB
+		a) Create database connection
+		b) Upload database
+		c) save struct to database
+	3) Comments
+		// this should be VERY similar to scraping the site
+		a) Get all comments
+		b) Transform into struct
+		c) Save
+*/
